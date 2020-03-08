4460387294d0987c3532e86add10ed96af898f61
mihailuncan13 <mihailuncan13@gmail.com>
2020-03-09 01:55:41 +0200

    Resized imagines, corrected text, added new YT link
    Resized imagines up to 270kb, corrected some text, added new YT - Unlisted item in Htag Project

--- src/utils/textUtil.js
+++ src/utils/textUtil.js
@@ -12,7 +12,7 @@ export const social = {
 export const textSocial = {
   about_small_text: 'Storytelling time',
   about_big_text: 'Mike Lunc Production',
-  about_info_text: 'Passion for storytelling and cinematography is what drives us. We started this company about two years ago with the goal to create as much quality content as possible. Our goal was to create pieces that would open another perspective in people s mind. So far, we achieved just that, with projects from all over the spectrum. We created videos that inspired industires, rally fans, traveles and many others. Join us in creating the world a more open minded place!',
+  about_info_text: "Passion for storytelling and cinematography is what drives us. We started this company about two years ago with the goal to create as much quality content as possible. Our goal was to create pieces that would open another perspective in people's mind. So far, we achieved just that, with projects from all over the spectrum. We created videos that inspired industires, rally fans, traveles and many others. Join us in creating the world a more open minded place!",
 }
 
 export const textServices = {
@@ -33,26 +33,26 @@ export const services = [
     id: 2,
     logo: 'camera-retro',
     title: 'Try photography out',
-    description: 'Sometimes a single photo can speak louder than a thousend words. We choose to believe that that is true. We want to bring that idea to life by providing any type of photography services. You give us a location, a product, a building, a coffee place or anything you can think of, and we create the photo around and with it. Imagine you want to shoot the moon for your clients, we will go to the moon and shoot it for y... Okay, maybe we can t do that, but you get where we are going with it.'
+    description: "Sometimes a single photo can speak louder than a thousend words. We choose to believe that that is true. We want to bring that idea to life by providing any type of photography services. You give us a location, a product, a building, a coffee place or anything you can think of, and we create the photo around and with it. Imagine you want to shoot the moon for your clients, we will go to the moon and shoot it for y... Okay, maybe we can't do that, but you get where we are going with it."
   },
   
   {
     id: 3,
     logo: 'lightbulb',
     title: 'Bring an idea to life',
-    description: 'Having just an idea, but not knowing how to implement it can be extremly frustrating, we all know that. That s why, when it comes to creativity, we can help with that. Meet us, tell us your idea and we will come up with a creative plan of how that idea could come to life. May that be an idea about a video, a photo, a design, a scribble, we can think creatively and come up with a plan to give it life. Sometimes a creative brain is weirdly fun to have around.'
+    description: "Having just an idea, but not knowing how to implement it can be extremly frustrating, we all know that. That's why, when it comes to creativity, we can help with that. Meet us, tell us your idea and we will come up with a creative plan of how that idea could come to life. May that be an idea about a video, a photo, a design, a scribble, we can think creatively and come up with a plan to give it life. Sometimes a creative brain is weirdly fun to have around."
   },
   {
     id: 4,
     logo: 'shoe-prints',
     title: 'Track your steps',
-    description: 'It is always important to keep track of your progress on the internet. When we speak about the internet, we actually mean social media. We want you to be able to focus on your daily tasks as a company and not having to worry about the social media content. That s why we offer a bundle of packs designed to help you with that. You can let us worry about what is being posted on your social media, about how to strategically create ads for your content, about how to reach as many people as possible and many others. Online Marketing can make or break the content if not applied correctly and we want to help with that. Especially if we created that contet for you.'
+    description: "It is always important to keep track of your progress on the internet. When we speak about the internet, we actually mean social media. We want you to be able to focus on your daily tasks as a company and not having to worry about the social media content. That's why we offer a bundle of packs designed to help you with that. You can let us worry about what is being posted on your social media, about how to strategically create ads for your content, about how to reach as many people as possible and many others. Online Marketing can make or break the content if not applied correctly and we want to help with that. Especially if we created that contet for you."
   },
 ]
 
 export const textProjects = {
   about_small_text: 'What have we done so far?',
-  about_big_text: 'A brief introduction in our PROJECTS',
+  about_big_text: 'A brief introduction in our Projects',
 }
 
 export const textContact = {
