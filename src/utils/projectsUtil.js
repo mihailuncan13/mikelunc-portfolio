7e60f496a32234494738c72ba3ba70b4db05e1a9
mihailuncan13 <mihailuncan13@gmail.com>
2020-03-15 20:12:25 +0200

    text proofing made - youtube links added

--- src/utils/projectsUtil.js
+++ src/utils/projectsUtil.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     id: 1,
     name: 'Ford Romania',
-    description: 'A project for Ford Romania in collaboration with Napoca Rally Academy, that was held in Iasi, Romania in 2019. The goal was to create a video that will show the online public how the entire event looks like. The event is called "Ford Driving Skills for Life" and is meant to teach, the people that signed up, how to drive safer. The event was held over the spam of two days. For this video, multiple cameras were used to highlight every step and the magnitute of the event.',
+    description: 'A project for Ford Romania in collaboration with Napoca Rally Academy, that took place in Iasi, Romania in 2019. The goal was to create a video that will showcase "Ford Driving Skills for Life" - a defensive driving event and workshop. The event was held over the span of two days. Due to the complexity, objects in motion and the multiple angles shot, we used several cameras to highlight the magnitude of the event.',
     cover: 'Ford_Romania/screen3_ford.jpg',
     images: [
       {
@@ -18,13 +18,13 @@ export const projects = [
         url: 'Ford_Romania/screen4_ford.jpg',
       },
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: "https://www.youtube.com/embed/hvlBCAXSTpA"
   },
   
   {
     id: 2,
     name: 'Energobit',
-    description: 'A project for Energobit, where the main focus is to highlight the products and the production of the products. The other main ingredient of the video was to show that the industry does not work without the people behind it. It is always a balance between robots and human, regardless of the field, and this video shows just that. The video was shot in the Energobit Cluj Factory in 2019',
+    description: 'Energobit wanted to introduce their offering - from conception to the end product. The secret driver of this project was the idea that the production machines are only as good as the people that steer them. We aspire to create a story that shows this balance between robots and human. The video was shot in the Energobit Cluj Factory in 2019',
     cover: 'Energobit/screen3_energo.jpg',
     images: [
       {
@@ -40,13 +40,13 @@ export const projects = [
         url: 'Energobit/screen4_energo.jpg',
       },
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: ""
   },
   
   {
     id: 3,
     name: 'Napoca Rally Academy 2019 Recap',
-    description: 'A project for Napoca Rally Academy highlighting the most important moments of the year 2019. After a season by their side, this was the most anticipated video of the year, where are the best footage was brought in the same place. The video is build with footage that was filmed all over Romania, at all the rallies where the team took part. It is important to mention that 2019 was a prolific year for NRA, as they won the national title. Congrats!',
+    description: 'A project for Napoca Rally Academy highlighting the most important moments of the year 2019. After accompanying them for a season, this was the most anticipated video of the year. because it brought all the greatest pieces together. It makes it a special piece because NRA als won the national title. Congrats!',
     cover: 'NRA/NRA_FINAL_2019/screen1_NRA_REZ_2019.jpg',
     images: [
       {
@@ -62,18 +62,15 @@ export const projects = [
         url: 'NRA/NRA_FINAL_2019/screen4_NRA_REZ_2019.jpg',
       },
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: "https://www.youtube.com/embed/9PNWYhJh4xE"
   },
   
   {
     id: 4,
     name: 'Napoca Rally Academy - Banca Transilvania',
-    description: 'A project for Napoca Rally Academy in collaboration with Banca Transilvania, that was filmed in Cluj-Napoca. The main focus of this video is to highlight the fact that paying with your mobile phone is possible when you have a credit card connected to your device. It was filmed with two of the most succesful rally drivers in Romania, Simone Tempestini - 4 times national champion and Bogdan Marisca - 2 times national champion.',
+    description: "A project for Napoca Rally Academy in collaboration with Banca Transilvania, filmed in Cluj-Napoca. The idea behind this project is BT's solution to ease of payment. Just connect your bank card to your phone. It was filmed with two of the most succesful rally drivers in Romania, Simone Tempestini - 4 times national champion and Bogdan Marisca - 2 times national champion.",
     cover: 'NRA/NRA_BT/screen3_NRA_BT.jpg',
     images: [
-      {
-        url: 'NRA/NRA_BT/screen1_NRA_BT.jpg',
-      },
       {
         url: 'NRA/NRA_BT/screen2_NRA_BT.jpg',
       },
@@ -82,13 +79,13 @@ export const projects = [
       },
 	 
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: "https://www.youtube.com/embed/5LGhJEIOUgc"
   },
   
   {
     id: 5,
     name: 'Simone Tempestini - WRC Recap 2019',
-    description: "A project made for Simone Tempestini, that was filmed throughout the year of 2019. The main focus of the video was to show the best moments of Simone's 2019 WRC season. This project was heavy on the editing as the footage amount was insanly high. In the end, it's a great feeling to look back at a year worth of material and deliever a highly energetic piece.",
+    description: "A video project made for Simone Tempestini, that was filmed throughout the year of 2019. The main focus of the video was to show Simone's 2019 best moments in the WRC season. This project was heavy on the editing as the footage amount was insanly high. In the end, it's a great feeling to look back at a year's worth of material that and deliver an energetic piece.",
     cover: 'Simone_Tempestini/WRC_Spain_Recap/screen2_WRC_Spain.jpg',
     images: [
       {
@@ -104,13 +101,13 @@ export const projects = [
         url: 'Simone_Tempestini/WRC_Spain_Recap/screen4_WRC_Spain.jpg',
       },
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: "https://www.youtube.com/embed/0MhtBCu10uA"
   },
   
   {
     id: 6,
     name: 'Wens Tour - Kenya 2019',
-    description: 'A project made for Wens Tour, that had the main focus to highlight a guided tour the company held in Kenya in 2019. The video was meant to create a nostalgic feeling for all the members that were on that tour. With the old-school graphics, that look was achieved. It was important to keep the footage as authentic as possible thus keeping the edit on a continous flow.',
+    description: 'A project designed for Wens Travel, for their Kenyan destination presentation. The video receives its personal and authentic touch from the user-generated content used. Therefore, the edit was also kept on a continuous flow.',
     cover: 'Wens/screen4_wens_kenya.jpg',
     images: [
       {
@@ -126,13 +123,13 @@ export const projects = [
         url: 'Wens/screen4_wens_kenya.jpg',
       },
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: "https://www.youtube.com/embed/AISiL2vf-mI"
   },
   
   {
     id: 7,
     name: 'Ready for Gravel',
-    description: 'A project personally made for Simone Tempestini, that was filmed in Brasov, Romania. This video was a short teaser for the upcoming season of 2019. The main focus was to highlight the new car, the weather contidions where the car testing took place and a brief look at the pilot itself, Simone. The goal was reached as the video reached a very high amount of people.',
+    description: "Simone Tempestini's personal project filmed in Brasov, Romania. This video was a short teaser for the upcoming season of 2019. The main focus was to highlight the new car, the weather conditions where the car testing took place and a brief look at the pilot. We managed to enthuse the desired audience in this presentation video.",
     cover: 'Simone_Tempestini/Ready_for_Gravel/screen1_simo_rdy_gravel.jpg',
     images: [
       {
@@ -148,13 +145,13 @@ export const projects = [
         url: 'Simone_Tempestini/Ready_for_Gravel/screen4_simo_rdy_gravel.jpg',
       },
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: "https://www.youtube.com/embed/T8qWIzXucA0"
   },
   
   {
     id: 8,
     name: 'Simone Tempestini - WRC Sardegna',
-    description: 'A project made for Simone Tempestini, that was filmed near Alghero, Sardinia, Italy, during the event of WRC Sardegna. All the footage was shot on a span of five days. The main focus was to create a high intensity video that would highlight the best moments of the crew at the event. The intesity in a rally car is always insanely high and this video shows just that. Above all, the ending is unexpected!',
+    description: 'A project made for Simone Tempestini filmed near Alghero, Sardinia, Italy, during the event of WRC Sardegna. All the footage was shot on a span of five days. The main focus was to create a high intensity video that would highlight the best moments of the crew at the event. The intesity in a rally car is always insanely high and this video shows just that. Above all, the ending is unexpected!',
     cover: 'Simone_Tempestini/WRC_Sardinia/screen4_WRC_Sardinia.jpg',
     images: [
       {
@@ -170,13 +167,13 @@ export const projects = [
         url: 'Simone_Tempestini/WRC_Sardinia/screen4_WRC_Sardinia.jpg',
       },
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: "https://www.youtube.com/embed/t_RASFMP3bY"
   },
   
   {
     id: 9,
     name: 'Hashtag Motorsport',
-    description: "A project made for Hashtag Motorsport, that was filmed in Brasov, Romania. The main focus of this video was to show what the company is all about. To highlight the abilities of the mechanics, the synergy they have while working and the complexity a rally service team can have. This video was purposed for the company's branding and is the main imagine piece as of 2020.",
+    description: "Hashtag Motorsport - company presentation video filmed in Brasov, Romania. Various aspects of their team-work and collaboration were the goal and it was proposed as the company's branding piece used as of 2020.",
     cover: 'Htag_Motorsport/screen1_htag.jpg',
     images: [
       {
@@ -198,7 +195,7 @@ export const projects = [
   {
     id: 10,
     name: 'Simone Tempestini - National Champion',
-    description: 'A project made with Simone Tempestini, that was filmed in Brasov, Romania and Barcelona, Spain. This is maybe the most exciting project made by us. The main focus of this video was to capture the moments before and after winning the national titile. It was also important to lay the groundwork for the upcoming video of the WRC Spain event where Simone would participate after TESS Rally Brasov.',
+    description: 'An international project (Brasov, Romania and Barcelona, Spain) of Simone Tempestini. This is maybe the most exciting project made by us. The main focus of this video was to capture the moments before and after winning the national title. It was also important to lay the groundwork for the upcoming presentation video of WRC Spain.',
     cover: 'Simone_Tempestini/CNR_Brasov/screen4_simo_arad_spain.jpg',
     images: [
       {
@@ -214,13 +211,13 @@ export const projects = [
         url: 'Simone_Tempestini/CNR_Brasov/screen4_simo_arad_spain.jpg',
       },
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: "https://www.youtube.com/embed/1UaHI1o81CA"
   },
   
   {
     id: 11,
     name: 'Napoca Rally Academy 2019 - Team Presentation',
-    description: "A project made for Napoca Rally Academy, that was filmed in Cluj-Napoca. The main focus of the video was to show the team lineup for the following season. Imporant parts of the video were to show the new equipment, a few items that are crucial to ralling and the crew itself. For this video, a set was build at the team's location.",
+    description: "Presentation video created for Napoca Rally Academy in Cluj-Napoca. We wanted to show the new equipment, a few items that are crucial to ralling and the awesome crew itself. We also built a set from scratch at their location for this shooting!",
     cover: 'NRA/NRA_Prezentare_2019/screen4_NRA_PREZ_2019.jpg',
     images: [
       {
@@ -236,13 +233,13 @@ export const projects = [
         url: 'NRA/NRA_Prezentare_2019/screen4_NRA_PREZ_2019.jpg',
       },
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: "https://www.youtube.com/embed/udGlq9vq368"
   },
   
   {
     id: 12,
     name: 'Skiing in Romania - Suior, Maramures',
-    description: 'A personal project made on a skiing weekend with some friends. It was filmed in Suior, Maramures on the skiing slopes. The main focus of this video was to capture the best moments of the skiing trip and be creative with the sole reason of  creating something for your own portofolio. The only gear used on this shoot was a handheld camera and a GoPro mounted on different objects. It is important to create for yourself and try new technique in the field and in editing and this project did just that.',
+    description: 'A personal project made on a skiing weekend with some friends. It was filmed in Suior, Maramures on the skiing slopes. No big creational purpose behind, just awesome time while increasing our content portfolio, using only a handheld camera and a GoPro. What better way to develop and try new techniques than by involving your friends?',
     cover: 'The_skiing_Project/screen_ski_3.jpg',
     images: [
       {
@@ -258,13 +255,13 @@ export const projects = [
         url: 'The_skiing_Project/screen_ski_4.jpg',
       },
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: "https://www.youtube.com/embed/xQDc5GjcbBI"
   },
   
   {
     id: 13,
     name: 'Transilvania Rally 2019',
-    description: 'A project made for Transilvania Rally 2019, with the main focus to create a short video that would show the public what Transilvania Rally is all about and how it was in the past. The video was played all over Cluj-Napoca transportation service as well as on the big screens during the rally. It was crucial to integrate the best moments from the past editions.',
+    description: 'Transilvania Rally 2019 - presentation video of the upcoming rally. Combining various techniques with historical footage, we portrayed the rally in its full magnitude and development throughout the years. Played all over Cluj-Napoca transportation services as well as on the big screens during the rally.',
     cover: 'NRA/NRA_Transilvania_Rally_2019/screen4_NRA_TRANSILVANIA.jpg',
     images: [
       {
@@ -280,13 +277,13 @@ export const projects = [
         url: 'NRA/NRA_Transilvania_Rally_2019/screen4_NRA_TRANSILVANIA.jpg',
       },
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: "https://www.youtube.com/embed/YAdWteq_mOk"
   },
   
   {
     id: 14,
     name: 'Simone Tempestini - WRC Germany',
-    description: "A project made for Simone Tempestini, that was filmed near Trier, Germany, during the event of WRC Germany. All the footage was shot on a span of five days. The main focus was to create a story around the event where Simone took place. To highlight the best moments as well as the struggles the crew had during the rally. It's a nailbiter! ",
+    description: "A project made for Simone Tempestini, that was filmed near Trier, Germany, during the event of WRC Germany. All the footage was shot on a span of five days. We wanted to show Simone's story in Germany - showing the best moments as well as the struggles the crew had during the rally. It's a nailbiter! ",
     cover: 'Simone_Tempestini/WRC_Germany/screen4_WRC_Germany.jpg',
     images: [
       {
@@ -302,13 +299,13 @@ export const projects = [
         url: 'Simone_Tempestini/WRC_Germany/screen4_WRC_Germany.jpg',
       },
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: "https://www.youtube.com/embed/vwc1s37rPFA"
   },
 	
   {
     id: 15,
     name: 'Simone Tempestini - WRC Portugal',
-    description: 'A project made for Simone Tempestini, that was filmed near Porto, Portugal, during the event of WRC Portugal. All the footage was shot on a span of five days. The main focus was to create a story around the event where Simone took place. The highlight was to show a more in depth view in the life of the crew Simone Tempestini - Sergiu Itu. It begins with the preparations and ends with the final of the rally. A whole story around the event. ',
+    description: 'A project made for Simone Tempestini, that was filmed near Porto, Portugal, during the event of WRC Portugal. All the footage was shot on a span of five days. This time the story was the crew Simone Tempestini - Sergiu Itu. All from the preparations and to the finale of the rally - a complete story. ',
     cover: 'Simone_Tempestini/WRC_Portugal/screen4_WRC_Portugal.jpg',
     images: [
       {
@@ -327,7 +324,7 @@ export const projects = [
         url: 'Simone_Tempestini/WRC_Portugal/screen5_WRC_Portugal.jpg',
       },
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: "https://www.youtube.com/embed/0ZZZktXVdeo"
   },
   
   {
@@ -352,7 +349,7 @@ export const projects = [
         url: 'Personal/screen5_poros.jpg',
       },
     ],
-    video: "https://www.youtube.com/embed/oOdCJa8nRv8"
+    video: ""
   },
 
 ]
